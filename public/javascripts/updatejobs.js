bb434c52ed43cf668c72dc2e513baef975e653cc
albert-magyar <albert.magyar@gmail.com>
2014-11-19 11:28:20 -0800

    Using js updates to populate job table.

--- public/javascripts/updatejobs.js
+++ public/javascripts/updatejobs.js
@@ -5,12 +5,23 @@
 	    dataType : 'json',
 	    success: function(data) {
 		console.log("AJAX Success");
+		$("#job_table .row").not(".header-row").remove();
+		dataByPointID = _.sortBy(data, function(point){ return point.pointID; });
+		_.map(dataByPointID, function(point){
+			var $div = $("<div>").addClass("row").attr("id",point.pointID);
+			$div.append($("<span>").addClass("cell").attr("data-label","Point ID").text(point.pointID));
+			$div.append($("<span>").addClass("cell").attr("data-label","Current Tool").text(point.currentTool));
+			$div.append($("<span>").addClass("cell").attr("data-label","Elapsed Time").text(point.elapsedTime));
+			$div.append($("<span>").addClass("cell").attr("data-label","Node ID").text(point.nodeID));
+			$div.append($("<span>").addClass("cell").attr("data-label","Child PID").text(point.childPID));
+			$("#job_table").append($div);
+		    });
 	    },
 	    error: function() {
                 console.log("AJAX Error");
 	    },
 	    complete: function() {
-		setTimeout(updateJobs, 5000);
+		setTimeout(updateJobs, 500);
 	    }
 	});
 })();
